2b8e97c942c077e0b067569199fa40cec830fcf3
Yury Shchegolev <cardiffc@yandex.ru>
2019-11-26 15:55:52 +0300

    JSON writing corrected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
@@ -11,32 +16,23 @@ public class Main {
     private static final String URL = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0" +
             "%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%" +
             "80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+
     private static final String OUTFILE = "src/main/out/test.json";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Parser metroParser = new Parser(URL);
         ArrayList<Station> stations = metroParser.parseStations();
         ArrayList<Line> lines = metroParser.parseLines(stations);
         ArrayList<ConnectionMos> connections = metroParser.parseConnections(URL,stations);
-//        connections.forEach(connection -> {
-//            System.out.println("#####################################################");
-//            for (Map.Entry<String, String> con : connection.getConnection().entrySet())
-//            {
-//
-//                System.out.println(con.getKey() + "/" + con.getValue());
-//            }
-//        });
-
-     createOut(lines,connections);
-
-
+        subwayToJson(lines,connections);
     }
-    public static void createOut (ArrayList<Line> lines, ArrayList<ConnectionMos> connections)
+    private static void subwayToJson(ArrayList<Line> lines, ArrayList<ConnectionMos> connections)
     {
         JSONObject  mapOfSubway = new JSONObject();
         JSONObject stationsToMap = new JSONObject();
         JSONArray linesToMap = new JSONArray();
+       //Lines and stations to JSON
         lines.forEach(line -> {
             JSONObject lineToObj = new JSONObject();
             JSONArray newline = new JSONArray();
@@ -44,39 +40,58 @@ public class Main {
             stations.forEach(station -> newline.add(station.getName()));
             stationsToMap.put(line.getNumber(),newline);
             lineToObj.put("number", line.getNumber());
-            lineToObj.put("name", line.getName());
             lineToObj.put("color", line.getColor());
+            lineToObj.put("name", line.getName());
             linesToMap.add(lineToObj);
         });
-
+        // Connections to JSON
         JSONArray connectionsToMap = new JSONArray();
+        JSONArray newConnection = new JSONArray();
+        JSONArray currentConnection = new JSONArray();
         connections.forEach(connection -> {
-            JSONArray newConnection = new JSONArray();
-            TreeMap<String, String> tempConnection = connection.getConnection();
-            JSONObject thisConnection = new JSONObject();
-            for (Map.Entry<String, String> con : tempConnection.entrySet())
+            newConnection.clear();
+            for (Map.Entry<String, String> con : connection.getConnection().entrySet())
             {
+                JSONObject thisConnection = new JSONObject();
                 thisConnection.put("line",con.getKey());
                 thisConnection.put("station",con.getValue());
                 newConnection.add(thisConnection);
             }
-
-            connectionsToMap.add(newConnection);
+            currentConnection.add(newConnection);
         });
+        connectionsToMap.add(currentConnection);
+
+        //Add Everything to global JSON object
         mapOfSubway.put("stations",stationsToMap);
-        mapOfSubway.put("connections", connectionsToMap);
         mapOfSubway.put("lines", linesToMap);
+        mapOfSubway.put("connections", connectionsToMap);
 
         writeToJson(mapOfSubway);
-
     }
-    public static void writeToJson (JSONObject map)
+    private static void writeToJson (JSONObject map)
     {
-        try (FileWriter file = new FileWriter(OUTFILE)) {
-            file.write(map.toJSONString());
-        } catch (IOException e) {
+        try {
+            FileOutputStream os=new FileOutputStream(OUTFILE,false);
+            BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String jsonOutput = gson.toJson(map);
+            bw.append(jsonOutput);
+            bw.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static String getJsonFile()
+    {
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(OUTFILE));
+            lines.forEach(line -> builder.append(line));
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return builder.toString();
+    }
 }
