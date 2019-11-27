347a9a6c3904d4d8ca653fb4893f947caa720e92
Yury Shchegolev <cardiffc@yandex.ru>
2019-11-27 14:37:04 +0300

    Release v1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
             "%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%" +
             "80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
 
-    private static final String OUTFILE = "src/main/resources/1/mosmetromap.json";
+    private static final String OUTFILE = "src/main/resources/mosmetromap.json";
 
     public static void main(String[] args) throws ParseException {
 
